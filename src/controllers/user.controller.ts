ad0bcb9e6c2453db14ba19d94c85ce6ffadb4a3a
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-08-16 12:05:52 +0330

    feat: user.controller implemnted

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,95 @@
+import {Request, Response, Router} from "express";
+import {UserService} from "../services/user.service";
+import {registerDto} from "../dtos/register.dto";
+import {loginDTO} from "../dtos/login.dto";
+
+export class UserController {
+    constructor(private readonly userService: UserService) {
+    }
+
+    async register(req: Request, res: Response) {
+        const result = registerDto.safeParse(req.body);
+
+        if (!result.success) {
+            return res.status(400).json({
+                success: false,
+                errors: 'input not valid'
+            });
+        }
+
+        const userData = result.data;
+
+        try {
+            const newUser = await this.userService.register(userData);
+            return res.status(201).json({
+                success: true,
+                message: "User created successfully",
+                user: {username: newUser.username, email: newUser.email},
+            });
+        } catch (error: any) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    }
+
+    async login(req: Request, res: Response) {
+        const result = loginDTO.safeParse(req.body);
+
+        if (!result.success) {
+            return res.status(400).json({
+                success: false,
+                errors: 'input not valid'
+            });
+        }
+
+        const userData = result.data;
+
+        try {
+            const loginUser = await this.userService.login(userData);
+            res.cookie('sessionId', loginUser.sessionId, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            })
+            res.cookie('refreshToken', loginUser.refreshToken, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            });
+
+            return res.status(200).json({
+                success: true,
+                message: "User logged in successfully",
+                data: {
+                    accessToken: loginUser.accessToken,
+                },
+            });
+        } catch (error: any) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    }
+
+    async refreshToken(req: Request, res: Response) {{
+        const { refreshToken } = req.cookies.get("refreshToken");
+        const{sessionId} = req.cookies.get("sessionId");
+
+        if (!refreshToken || !sessionId) {
+            return res.status(401).json({error: 'No refresh token or sessionId'});
+        }
+
+        const newAccessToken = await this.userService.refreshToken(sessionId,refreshToken)
+        return res.status(200).json({
+            success: true,
+            message: "Refresh token successfully",
+            data: {
+                accessToken: newAccessToken,
+            }
+        })
+
+    }}
+}
