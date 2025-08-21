95782b3a442e857b30c92c95da30ed4de66846a3
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-08-21 10:39:38 +0330

    feat: uploadProfilePhoto middleware implemented

--- src/middlewares/uploadProfilePhoto.middleware.ts
+++ src/middlewares/uploadProfilePhoto.middleware.ts
@@ -1,1 +1,22 @@
+import path from 'path';
+import multer, { StorageEngine } from 'multer';
+import { Request } from 'express';
+import * as fs from "node:fs";
 
+const uploadPath = path.join(__dirname, '/uploads');
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
+const storage: StorageEngine = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb) => {
+        cb(null, path.join(__dirname, '/uploads'));
+    },
+    filename: (req: Request, file: Express.Multer.File, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    },
+});
+
+export const uploadProfilePhotoMiddleware = multer({ storage });
